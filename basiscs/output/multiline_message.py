901dd9bf1af49a67e40aef2fbfb6248deef353a5
repl.it user <replituser@example.com>
2021-02-26 11:17:25 +0000

    Addet mesage to output

---
+++ basiscs/output/multiline_message.py
@@ -0,0 +1,7 @@
+#Display message to standard output in multiple lines of code
+print("system failure iminent!")
+print ("system reboot has beesn initiated...\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hoover engine\n")
+print ("System online")
